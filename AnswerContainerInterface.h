89d7d362067ead4a3d1d45b3726163356a65dce2
PaulMoroz <mpasa4186@gmail.com>
2021-09-17 20:01:34 +0300

    Refactored naming

---
+++ AnswerContainerInterface.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+#include "IRequests/IRequests.h"
+#include "MSSQLDatabase.h"
+using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener;
+extern class IRequests;
+
+class AnswerContainerInterface {
+protected:
+	http_request request;
+	IRequests* requestProcessor;
+	json::value answer;
+	status_code code;
+	bool done = false;
+public:
+	AnswerContainerInterface(http_request request, IRequests* requestProcessor);
+	virtual void ProcessRequest(){}
+	virtual void RespondOnRequest(){}
+	virtual void SetAnswer(json::value answer) {}
+	virtual void SetStatusCode(status_code code) {}
+	bool IsDone();
+	void MakeDone();
+};
